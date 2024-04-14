a69c2e23d741868c68c19f09220e884ad9256661
smriti <smritikhanal45@gmail.com>
2024-04-14 12:45:48 +0545

    Week 1

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -4,6 +4,7 @@ public class Hello{
     public static void main(String[] args) {
         /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("Hello World!");
+
         /*2. Extend/Modify the above program to print two additional lines:
 
 This is my first program
@@ -12,7 +13,32 @@ I am on module ST4003CEM
 
 System.out.print (not println) and produces the same output. */
 System.out.print("This is my first program\n");
-System.out.print("I am a module student");
+System.out.print("I am a module student\n");
+
+/*3. Write a program that prints the following output:
+
+A "quoted" String is
+
+'much' better if you learn
+
+the rules of "escape sequences."
+
+Also, "" represents an empty
+
+String. Don't forget: use \"
+
+instead of " !
+
+'' is not the same as " */
+System.out.println("A \"quoted\" String is");
+System.out.println("\'much\' better if you learn");
+System.out.println("the rules of \"escape sequences.\"");
+System.out.println("Also, \"\"represents an empty");
+System.out.println("String. Don't forget: use\\");
+System.out.println("instead of \"!");
+System.out.println("\" is not the same as \"");
+
+
     }
 
 }
