bcec66d279d03a41f7e841d676abb62acc790505
smriti <smritikhanal45@gmail.com>
2024-04-14 12:24:04 +0545

    Week 1 comment

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -4,6 +4,15 @@ public class Hello{
     public static void main(String[] args) {
         /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("Hello World!");
+        /*2. Extend/Modify the above program to print two additional lines:
+
+This is my first program
+
+I am on module ST4003CEM
+
+System.out.print (not println) and produces the same output. */
+System.out.print("This is my first program\n");
+System.out.print("I am a module student");
     }
 
 }
